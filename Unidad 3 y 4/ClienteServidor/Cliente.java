e2ae96a9731af8174301756c0f8af713386a0cc2
Mario Ledezma <133705185+MarioLedezma@users.noreply.github.com>
2023-07-30 20:33:30 -0600

    Update Cliente.java

--- Unidad 3 y 4/ClienteServidor/Cliente.java
+++ Unidad 3 y 4/ClienteServidor/Cliente.java
@@ -1,1 +1,45 @@
+package ClienteServidor;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+
+
+public class Cliente implements Runnable{
+
+     //CREAR LOS ATRIBUTOS DE PUERTO Y MENSAJE
+    private int puerto;
+    private String mensaje;
+
+    public Cliente(int puerto, String mensaje) {
+        this.puerto = puerto;
+        this.mensaje = mensaje;
+    }
+       //Es la interface de Java que dispone de un método run 
+       //y nos permite ejecutar una Tarea en paralelo desde un programa main usando la clase Thread.     
+            
+    @Override
+    public void run() {
+     //HOST DEL SERVIDOR
+     final String HOST ="127.0.0.1";
+     
+     //PUERTO DEL SERVIDOR
+     DataOutputStream salida;
+     
+     try{
+         Socket socket = new Socket (HOST, puerto);
+         salida = new DataOutputStream(socket.getOutputStream());
+         
+         //ENVIAMOS EL MENSAJE
+         salida.writeUTF(mensaje);
+         
+     }catch(IOException error){
+         System.out.println(error);
+     }
+     
+    }
+    
+    
+    
+}
